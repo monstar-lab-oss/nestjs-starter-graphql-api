78475b5c26550f36756f25c5ebed0d5fc4d8d38b
S M Asad Rahman <asad.rahman@monstar-lab.com>
2021-07-06 15:27:48 +0600

    feat: removed swagger tools

--- src/article/dtos/author-output.dto.ts
+++ src/article/dtos/author-output.dto.ts
@@ -1,13 +1,9 @@
 import { Expose } from 'class-transformer';
 
-import { ApiProperty } from '@nestjs/swagger';
-
 export class AuthorOutput {
   @Expose()
-  @ApiProperty()
   id: number;
 
   @Expose()
-  @ApiProperty()
   name: string;
 }
--- src/auth/dtos/auth-register-input.dto.ts
+++ src/auth/dtos/auth-register-input.dto.ts
@@ -5,28 +5,23 @@ import {
   IsString,
   IsEmail,
 } from 'class-validator';
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { ROLE } from '../constants/role.constant';
 
 export class RegisterInput {
-  @ApiProperty()
   @IsNotEmpty()
   @MaxLength(100)
   @IsString()
   name: string;
 
-  @ApiProperty()
   @MaxLength(200)
   @IsString()
   username: string;
 
-  @ApiProperty()
   @IsNotEmpty()
   @Length(6, 100)
   @IsString()
   password: string;
 
-  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   @MaxLength(100)
